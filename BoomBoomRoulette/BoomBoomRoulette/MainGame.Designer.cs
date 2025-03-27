5714abe0c0cbabba6808037a93d26e262186e1bc
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-03-27 09:03:08 -0700

    Worked on the Ammo Loading Logic and the game settings UI

--- BoomBoomRoulette/BoomBoomRoulette/MainGame.Designer.cs
+++ BoomBoomRoulette/BoomBoomRoulette/MainGame.Designer.cs
@@ -29,42 +29,99 @@
         private void InitializeComponent()
         {
             EventsBox = new ListBox();
-            aimPlayerBtn = new Button();
+            StartRoundBtn = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            GSTLabel = new Label();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // EventsBox
             // 
             EventsBox.FormattingEnabled = true;
             EventsBox.ItemHeight = 25;
-            EventsBox.Location = new Point(398, 59);
+            EventsBox.Location = new Point(786, 69);
             EventsBox.Name = "EventsBox";
-            EventsBox.Size = new Size(351, 379);
+            EventsBox.Size = new Size(462, 504);
             EventsBox.TabIndex = 0;
             // 
-            // aimPlayerBtn
+            // StartRoundBtn
             // 
-            aimPlayerBtn.Location = new Point(12, 130);
-            aimPlayerBtn.Name = "aimPlayerBtn";
-            aimPlayerBtn.Size = new Size(112, 34);
-            aimPlayerBtn.TabIndex = 1;
-            aimPlayerBtn.Text = "button1";
-            aimPlayerBtn.UseVisualStyleBackColor = true;
+            StartRoundBtn.Location = new Point(12, 112);
+            StartRoundBtn.Name = "StartRoundBtn";
+            StartRoundBtn.Size = new Size(148, 34);
+            StartRoundBtn.TabIndex = 1;
+            StartRoundBtn.Text = "Start Game";
+            StartRoundBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(409, 59);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(103, 59);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 3;
+            // 
+            // GSTLabel
+            // 
+            GSTLabel.AutoSize = true;
+            GSTLabel.Location = new Point(12, 22);
+            GSTLabel.Name = "GSTLabel";
+            GSTLabel.Size = new Size(127, 25);
+            GSTLabel.TabIndex = 4;
+            GSTLabel.Text = "Game Settings";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(259, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Health Per Player";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(25, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Rounds";
             // 
             // MainGame
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(aimPlayerBtn);
+            ClientSize = new Size(1265, 635);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(GSTLabel);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(StartRoundBtn);
             Controls.Add(EventsBox);
             Name = "MainGame";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ListBox EventsBox;
-        private Button aimPlayerBtn;
+        private Button StartRoundBtn;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label GSTLabel;
+        private Label label1;
+        private Label label2;
     }
 }
--- BoomBoomRoulette/BoomBoomRoulette/MainGame.cs
+++ BoomBoomRoulette/BoomBoomRoulette/MainGame.cs
@@ -12,8 +12,33 @@ namespace BoomBoomRoulette
         int computerHealth = 0;
 
         // function that Generates a random number to be used throghtout the application
-        int generateRandomNumber(int input) {
-            return 0;
+        int generateRandomNumber(int ranMax) {
+            Random ran = new Random();
+            // Generates a random number that is between 0 and the ranMax
+            // The generated number is then stored in the outputNum variable
+            int outputNum = ran.Next(0, ranMax);
+            return outputNum;
         }
+
+        // Function that will "load the gun with a random assortment of shells that are either live or not live"
+        void loadGun() {
+            // Loop that will load the gun by adding items to the ammoLoaded array
+            for (int i = 0; i < 5; i++) {
+                // Calls the generateRandomNumer function, and passes 2 into the function
+                // The function should return either a 0 or 1, thus determining if the shell loaded is considered "live or not"
+                int roundToLoad = generateRandomNumber(2);
+                if (roundToLoad == 0)
+                {
+                    ammoLoaded = ammoLoaded.Append("live").ToArray();
+                }
+                else {
+                    ammoLoaded = ammoLoaded.Append("live").ToArray();
+
+                }
+            
+            }
+        }
+
+        void showLoadedRounds() { }     
     }
 }
